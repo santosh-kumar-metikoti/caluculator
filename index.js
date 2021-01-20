c75d488e2db705cb6bf8d3c4783b565c9bbc49b8
santosh-kumar-metikoti <santosh.metikoti@gmail.com>
2021-01-21 02:14:09 +0530

    caluculator

---
+++ index.js
@@ -0,0 +1,27 @@
+
+        function display(val) 
+        { 
+           document.getElementById("result").value+=val 
+        } 
+          
+        function solve() 
+        { 
+            let x = document.getElementById("result").value 
+            let y = eval(x) 
+            document.getElementById("result").value = y 
+        } 
+    
+        function clr() 
+        { 
+            document.getElementById("result").value = "" 
+        } 
+
+        function isInputNumber(evt){
+                
+            var ch = String.fromCharCode(evt.which);
+            
+            if(!(/[0-9]/.test(ch))){
+                evt.preventDefault();
+            }
+            
+        }
